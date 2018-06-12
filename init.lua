eaaab5e1fc45ccc486c1e9752150fb4375f95fcc
Alex Henry <javelinrl@gmail.com>
2018-06-12 16:17:40 -0300

    Adding command privilege

--- init.lua
+++ init.lua
@@ -1,7 +1,4 @@
-local DEBUG=true;
-
 path=minetest.get_modpath("minerpg")
-
 dofile(path..'/src/items.lua')
 dofile(path..'/src/rpg.lua')
 dofile(path..'/src/prices.lua')
@@ -9,7 +6,4 @@ dofile(path..'/src/merchant.lua')
 dofile(path..'/src/wizard.lua')
 dofile(path..'/src/fighter.lua')
 dofile(path..'/src/trainer.lua')
-
-if DEBUG then
-    dofile(path..'/src/commands.lua')
-end
+dofile(path..'/src/commands.lua')
--- src/commands.lua
+++ src/commands.lua
@@ -1,25 +1,28 @@
+minetest.register_privilege("rpg", {description="Enables minerpg commands",give_to_singleplayer=true})
+local privs={rpg=true}
+
 -- skips a day
-minetest.register_chatcommand("day", {func=function(name,param)
+minetest.register_chatcommand("day",{privs=privs,func=function(name,param)
     minetest.set_timeofday(1)
     return true,'ok'
 end})
 
-minetest.register_chatcommand("spawnwizard", {func=function(name,param)
+minetest.register_chatcommand("spawnwizard",{privs=privs,func=function(name,param)
     spawn('minerpg:wizard',minetest.get_player_by_name(name):get_pos())
     return true,'ok'
 end})
 
-minetest.register_chatcommand("spawnfighter", {func=function(name,param)
+minetest.register_chatcommand("spawnfighter",{privs=privs,func=function(name,param)
     spawn('minerpg:fighter',minetest.get_player_by_name(name):get_pos())
     return true,'ok'
 end})
 
-minetest.register_chatcommand("spawnmerchant", {func=function(name,param)
+minetest.register_chatcommand("spawnmerchant",{privs=privs,func=function(name,param)
     spawn('minerpgg:merchant',minetest.get_player_by_name(name):get_pos())
     return true,'ok'
 end})
 
-minetest.register_chatcommand("spawnhub", {func=function(name,param)
+minetest.register_chatcommand("spawnhub",{privs=privs,func=function(name,param)
     spawnhub(minetest.get_player_by_name(name):get_pos())
     return true,'ok'
 end})
